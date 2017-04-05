b9bfa5474d3aff1d564de093eab63c47ee75437c
= <bkdas@teamdrg.com>
2017-04-06 01:18:10 +0530

    added MVC Struct.

---
+++ model/admin.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');  
+var adminSchema = new mongoose.Schema({  
+  name: String,
+  badge: Number,
+  dob: { type: Date, default: Date.now },
+  isloved: Boolean
+});
+
+mongoose.model('Admin', adminSchema);
